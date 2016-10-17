0bd3b5548f9f3e3accc3af8c409ff42bfea817b0
Daiki Hirasawa <flat053117117@gmail.com>
2016-10-17 15:40:43 +0900

    structにアクセスするのはpublicが必要だとわかりました ちょっとだけコード追加

--- TeamZenmaiSiki/Assets/Scripts/Search/EnemyDataReader.cs
+++ TeamZenmaiSiki/Assets/Scripts/Search/EnemyDataReader.cs
@@ -6,20 +6,25 @@ public class EnemyDataReader : MonoBehaviour {
 	// Use this for initialization
     public struct EnemyData
     {
-        Vector2 trancePosition;
-        Vector2 scale;
-        EnemyType.EnemyTypes enemyType;
-        string[] texts;
+        public Vector2 trancePosition;
+        public Vector2 scale;
+        public EnemyType.EnemyTypes enemyType;
+        public string[] texts;
     }
+
     private EnemyData enemyData;
 
     public EnemyData getEnemyData
     {
         get { return enemyData; }
     }
+    public void setEnemyData(EnemyData _enemyData)
+    {
+        enemyData = _enemyData;
+    }
 
-	void Start () {
-	
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
