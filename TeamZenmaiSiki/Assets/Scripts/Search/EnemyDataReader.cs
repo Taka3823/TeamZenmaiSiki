3cbd7182ac1d4eee52f00a42b97eeccde25793b0
Daiki Hirasawa <flat053117117@gmail.com>
2016-10-17 13:39:21 +0900

    エネミーのスポーナー、データロードなどのスクリプトを用意

---
+++ TeamZenmaiSiki/Assets/Scripts/Search/EnemyDataReader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDataReader : MonoBehaviour {
+
+	// Use this for initialization
+    public struct EnemyData
+    {
+        Vector2 trancePosition;
+        Vector2 scale;
+        EnemyType.EnemyTypes enemyType;
+        string[] texts;
+    }
+    private EnemyData enemyData;
+
+    public EnemyData getEnemyData
+    {
+        get { return enemyData; }
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
