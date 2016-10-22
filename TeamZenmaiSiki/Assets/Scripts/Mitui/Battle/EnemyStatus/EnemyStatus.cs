8df0406fa841bcd4069c127539134680c7d238fb
YoshiDaizu <yosshy_mi_kun@yahoo.co.jp>
2016-10-22 15:52:40 +0900

    マージ失敗を防ぐためのテスト

---
+++ TeamZenmaiSiki/Assets/Scripts/Mitui/Battle/EnemyStatus/EnemyStatus.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class EnemyStatus : MonoBehaviour {
+
+    public struct CSVData
+    {
+        public int HP;
+        public int ATK;
+    }
+
+    const int CSVDATA_ELEMENTS = 2;
+
+    private CSVData[] csvData;
+
+    public void ReadFile()
+    {
+        string path = Application.dataPath + "/CsvTest.csv";
+
+        string[] lines = ReadCsvFoundation.ReadCsvData(path);
+
+        csvData = new CSVData[lines.Length];
+        
+
+        string[]  didCommaSeparationData = new string[lines.Length];
+
+        char[] commaSpliter = { ',' };
+        
+        for(int i = 0; i < lines.Length; i++)
+        {
+            didCommaSeparationData = ReadCsvFoundation.DataSeparation(lines[i], commaSpliter, CSVDATA_ELEMENTS);
+        }
+
+        Debug.Log(csvData[0]);
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        ReadFile();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log("Hit!");
+    }
+}
