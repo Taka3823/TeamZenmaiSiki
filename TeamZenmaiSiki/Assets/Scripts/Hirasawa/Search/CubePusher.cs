c75ac8acd6332c2a9f696545bc3e587758cd06c7
Daiki Hirasawa <flat053117117@gmail.com>
2016-10-24 12:27:02 +0900

    敵をCSVで配置、三井さんが必要な構造体を用意

---
+++ TeamZenmaiSiki/Assets/Scripts/Hirasawa/Search/CubePusher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubePusher : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject cubeTest;
+
+    // Use this for initialization
+    void Start()
+    {
+        string[] str = ReadCsvFoundation.ReadCsvData(Application.dataPath + "/CSVFiles/Search/Episode1/Stage1/Unit1.csv");
+        char[] commaSpliter = { ',' };
+        string[] str2 = ReadCsvFoundation.DataSeparation(str[0],commaSpliter,3);
+        
+        //GameObject obj = Instantiate(cubeTest, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0)) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- TeamZenmaiSiki/Assets/Scripts/Hirasawa/Search/EnemyDataReader.cs
+++ TeamZenmaiSiki/Assets/Scripts/Hirasawa/Search/EnemyDataReader.cs
@@ -1,34 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+public class EnemyDataReader : MonoBehaviour
+{
 
-public class EnemyDataReader : MonoBehaviour {
+    // Use this for initialization
+   
 
-	// Use this for initialization
-    public struct EnemyData
-    {
-        public Vector2 trancePosition;
-        public Vector2 scale;
-        public EnemyType.EnemyTypes enemyType;
-        public string[] texts;
-    }
+    private List<List<EnemyData.EnemyInternalDatas>> enemyData = new List<List<EnemyData.EnemyInternalDatas>>();
 
-    private EnemyData enemyData;
+    
 
-    public EnemyData getEnemyData
+    public List<List<EnemyData.EnemyInternalDatas>> GetEnemyData
     {
         get { return enemyData; }
     }
-    public void setEnemyData(EnemyData _enemyData)
+
+    public void setEnemyData(List<List<EnemyData.EnemyInternalDatas>> _enemyData)
     {
+
         enemyData = _enemyData;
     }
 
-    void Start () {
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ReadData(int episodeNum, int stageNum, int unitNumber)
+    {
+        //ステージまでのパスを取得
+        string pass = Application.dataPath +
+            "/CSVFiles/Search/Episode" + episodeNum.ToString() + "/Stage" + stageNum.ToString() + "/";
+
+        string[] str = ReadCsvFoundation.ReadCsvData(pass + "Unit" + (unitNumber).ToString() + ".csv");
+        char[] commaSpliter = { ',' };
+
+        List<EnemyData.EnemyInternalDatas> line = new List<EnemyData.EnemyInternalDatas>();
+        for (int i = 0; i < str.Length; i++)//そのユニット内の敵の数だけデータを取得
+        {
+            string[] str2 = ReadCsvFoundation.DataSeparation(str[i], commaSpliter, 3);
+            //[Unit1の][２番目の敵]など・・・
+            EnemyData.EnemyInternalDatas enemybuf = new EnemyData.EnemyInternalDatas();
+            enemybuf.name = str2[0];
+            line.Add(enemybuf);
+        }
+
+        enemyData.Add(line);
+    }
 }
