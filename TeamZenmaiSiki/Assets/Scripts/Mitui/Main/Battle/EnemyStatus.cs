fe785c41269466eed8db497cae2b91aee6fe4021
YoshiDaizu <yosshy_mi_kun@yahoo.co.jp>
2016-10-29 12:30:11 +0900

    Revert "Lineでの変更点"
    This reverts commit 6adfbe61a8ae4f87ca2dd0bd98f9c786e9ca40bb.

---
+++ TeamZenmaiSiki/Assets/Scripts/Mitui/Main/Battle/EnemyStatus.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class EnemyStatus : MonoBehaviour {
+
+    public struct CSVData
+    {
+        string Name;
+        public int Hp;
+        public int Atk;
+        string Group;
+    }
+
+    const int CSVDATA_ELEMENTS = 4;
+
+    private CSVData[] csvData;
+
+    public void ReadFile()
+    {
+        string path = Application.dataPath + "/CsvTest.csv";
+
+        string[] lines = ReadCsvFoundation.ReadCsvData(path);
+
+        csvData = new CSVData[lines.Length];
+        
+
+        string[]  didCommaSeparationData = new string[lines.Length];
+
+        char[] commaSpliter = { ',' };
+        
+        for(int i = 0; i < lines.Length; i++)
+        {
+            didCommaSeparationData = ReadCsvFoundation.DataSeparation(lines[i], commaSpliter, CSVDATA_ELEMENTS);
+            Debug.Log(didCommaSeparationData[0]);
+        }
+
+       
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        ReadFile();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log("Hit!");
+    }
+}
---
+++ TeamZenmaiSiki/Assets/Scripts/Mitui/Main/Battle/PlayerStatus.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatus : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "AttackCircle")
+        {
+            Debug.Log("Hit!");
+            Destroy(this.gameObject);
+        }
+    }
+}
