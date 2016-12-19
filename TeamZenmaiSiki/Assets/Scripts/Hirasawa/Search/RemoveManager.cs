0a3bfe63cf4011e85565bed9e6c998c318cc707a
Daiki Hirasawa <flat053117117@gmail.com>
2016-12-19 17:00:41 +0900

    ぷｓっしゅ

---
+++ TeamZenmaiSiki/Assets/Scripts/Hirasawa/Search/RemoveManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveManager : MonoBehaviour {
+
+    private static RemoveManager instance;
+
+    public static RemoveManager Instance
+    {
+        get { return instance; }
+    }
+    // Use this for initialization
+    void Start () {
+        instance = this;
+        int a = DataManager.Instance.ScenarioChapterNumber;
+        int b = DataManager.Instance.ScenarioSectionNumber;
+        Debug.Log(a);
+        Debug.Log(b);
+        //datas = DataManager.Instance.DirectiveDatas[a][b];
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            for (int i = 0; i < DataManager.Instance.KillNames.Count; i++)
+            {
+                Debug.Log(DataManager.Instance.KillNames[i] + i);
+            }
+            Debug.Log(DataManager.Instance.KillNum);
+        }
+	}
+}
