ca35e91ae112bb3269d0b9f069e3bb85adfefb46
Daiki Hirasawa <flat053117117@gmail.com>
2017-01-10 02:41:55 +0900

    フェードアウト関係

---
+++ TeamZenmaiSiki/Assets/Fade/Scripts/FadeTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeTest : MonoBehaviour
+{
+    [SerializeField]
+    Fade fade = null;
+    bool baka = false;
+    public void FadeOuts()
+    {
+        baka = false;
+        Debug.Log(baka);
+        fade.FadeIn(5, () =>
+        {
+            testdayo();
+        });
+    }
+    public void OnClick()
+    {
+        fade.FadeIn(1, () =>
+        {
+            fade.FadeOut(1);
+        });
+    }
+    public void testdayo()
+    {
+        baka = true;
+        Debug.Log(baka);
+    }
+}
