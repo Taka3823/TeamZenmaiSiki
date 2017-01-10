97a9bf8705af1fd946b132ebca7a514b2e924cea
PS15A_Iwano\vantan <a.lb.3823cm@gmail.com>
2017-01-10 23:07:59 +0900

    シナリオ選択画面を雑に改修

--- TeamZenmaiSiki/Assets/Scripts/Iwano/ScenarioChoice/SliedPanel.cs
+++ TeamZenmaiSiki/Assets/Scripts/Iwano/ScenarioChoice/SliedPanel.cs
@@ -6,13 +6,64 @@ public class SliedPanel : MonoBehaviour
     [SerializeField]
     GameObject panel;
 
+    [SerializeField]
+    GameObject leftButton;
+
+    [SerializeField]
+    GameObject rightButton;
+
+    int count = 0;
+
+    void Start()
+    {
+        if(count <= 0)
+        {
+            leftButton.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if(count > 0)
+        {
+            leftButton.SetActive(true);
+        }
+
+        if(count < 9)
+        {
+            rightButton.SetActive(true);
+        }
+    }
+
     public void OnClickLeft()
     {
+        if (count <= 0)
+        {
+            return;
+        }
+
+        count -= 1;
         panel.transform.position = new Vector3(panel.transform.position.x + 1334, panel.transform.position.y, panel.transform.position.z);
+
+        if(count <= 0)
+        {
+            leftButton.SetActive(false);
+        }
     }
 
     public void OnClickRight()
     {
+        if(count >= 9)
+        {
+            return;
+        }
+
+        count += 1;
         panel.transform.position = new Vector3(panel.transform.position.x - 1334, panel.transform.position.y, panel.transform.position.z);
+
+        if(count >= 9)
+        {
+            rightButton.SetActive(false);
+        }
     }
 }
